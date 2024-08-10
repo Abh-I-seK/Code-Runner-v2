dd247da4ffda5716b1ea196aeb05cbf66b5258fd
Abh-I-sek <abhisek211004@gmail.com>
2024-08-10 17:13:55 +0530

    removed analytics and stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,8 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics/>
-        <SpeedInsights/>
       </body>
     </html>
   );
